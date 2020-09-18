9821b1f8e461e8c8aaf5aa41b1878f59eebb1f7e
Mike Blaszczak <mike@blaszczak.com>
2020-09-18 10:23:44 -0700

    add modefactory, hook up mode switching (almost works)

---
+++ stoplightd/modefactory.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include "regularlightmode.h"
+#include "selectormode.h"
+
+class ModeFactory
+{
+public:
+   static ModeInterface* FromModeNumber(int n, Lights& lights, daemonize::logger& log)
+   {
+      ModeInterface* p = nullptr;
+
+      switch (n)
+      {
+         case 0:
+            p = new SelectorMode(lights, log);
+            break;
+
+         case 1:
+            p = new RegularLightMode(lights, log);
+            break;
+
+         default:
+            log << log.critical << "Invalid mode number: " << n << std::endl;
+      }
+
+      return p;
+   }
+
+};
+
