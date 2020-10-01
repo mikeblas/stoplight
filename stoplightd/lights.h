25f8e716ee9217569fe041f60888d731ec782232
Mike Blaszczak <mike@blaszczak.com>
2020-10-01 15:11:00 -0700

    add support for indexed toggling, indicator lights

--- stoplightd/lights.h
+++ stoplightd/lights.h
@@ -9,13 +9,27 @@ class Lights {
     struct gpiod_line* lineRed;
     struct gpiod_line* lineYellow;
     struct gpiod_line* lineGreen;
+    struct gpiod_line* lineInd1;
+    struct gpiod_line* lineInd2;
 
 public:
+   enum LightLine {
+      LIGHT_RED,
+      LIGHT_YELLOW,
+      LIGHT_GREEN,
+
+      LIGHT_IND_1,
+      LIGHT_IND_2,
+   };
+
    Lights(const char* clientname, struct gpiod_chip* chip)
    {
       lineRed  = gpiod_chip_get_line(chip, 21);
       lineYellow  = gpiod_chip_get_line(chip, 20);
       lineGreen  = gpiod_chip_get_line(chip, 16);
+
+      lineInd1 = gpiod_chip_get_line(chip, 12);
+      lineInd2 = gpiod_chip_get_line(chip, 25);
 
       gpiod_line_request_output(lineRed, clientname, 0);
       gpiod_line_request_output(lineYellow, clientname, 0);
@@ -27,6 +41,9 @@ public:
       SetRed(0);
       SetYellow(0);
       SetGreen(0);
+
+      SetInd1(0);
+      SetInd2(0);
    }
 
    void AllOn()
@@ -34,6 +51,9 @@ public:
       SetRed(1);
       SetYellow(1);
       SetGreen(1);
+
+      SetInd1(1);
+      SetInd2(1);
    }
 
    void SetRed(int state)
@@ -49,6 +69,16 @@ public:
    void SetGreen(int state)
    {
       gpiod_line_set_value(lineGreen, state);
+   }
+
+   void SetInd1(int state)
+   {
+      gpiod_line_set_value(lineInd1, state);
+   }
+
+   void SetInd2(int state)
+   {
+      gpiod_line_set_value(lineInd2, state);
    }
 
    void ToggleRed()
@@ -67,6 +97,44 @@ public:
    {
       int state = gpiod_line_get_value(lineGreen);
       SetGreen(!state);
+   }
+
+   void ToggleInd1()
+   {
+      int state = gpiod_line_get_value(lineInd1);
+      SetInd1(!state);
+   }
+
+   void ToggleInd2()
+   {
+      int state = gpiod_line_get_value(lineInd2);
+      SetInd2(!state);
+   }
+
+   void Toggle(enum LightLine light)
+   {
+      switch (light)
+      {
+         case LIGHT_RED:
+            ToggleRed();
+            break;
+
+         case LIGHT_GREEN:
+            ToggleGreen();
+            break;
+
+         case LIGHT_YELLOW:
+            ToggleYellow();
+            break;
+
+         case LIGHT_IND_1:
+            ToggleInd1();
+            break;
+
+         case LIGHT_IND_2:
+            ToggleInd2();
+            break;
+      }
    }
 
    void FlickAll(int count, int millis) {
